af9601b05588905d4536928c82323f78349771b2
Nathan Baldez <nathanbsb2@hotmail.com>
2025-07-14 17:37:51 -0300

    chore: configuração do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+
+	const config = new DocumentBuilder()
+		.setTitle('CRM Jurídico')
+		.setDescription('application protocol inferface of CRM Jurídico')
+		.setVersion('1.0')
+		.build()
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('/api-docs', app, documentFactory)
+
+	await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
