087e48b2397cf003b2c5e830ee3ec5d6927205f7
Nathan Baldez <nathanbsb2@hotmail.com>
2025-07-19 15:48:44 -0300

    feat: criação da configuração do JWT e Passport no Nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { AuthModule } from './infra/auth/auth.module'
 import { PrismaService } from './infra/database/prisma/prisma.service'
 import { envSchema } from './infra/env'
 
@@ -10,6 +11,7 @@ import { envSchema } from './infra/env'
 			validate: (env) => envSchema.parse(env),
 			isGlobal: true,
 		}),
+		AuthModule,
 	],
 	controllers: [CreateAccountController],
 	providers: [PrismaService],
