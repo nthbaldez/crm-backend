51b0bd45bc31c0a1c0d6d8c1ec88f44bf531711d
Nathan Baldez <nathanbsb2@hotmail.com>
2025-07-19 22:18:08 -0300

    feat: validação dos dados de login no AuthenticateController

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,15 +1,64 @@
-import { Controller, Post } from '@nestjs/common'
+import {
+	Body,
+	Controller,
+	HttpCode,
+	Post,
+	UnauthorizedException,
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { ApiOperation } from '@nestjs/swagger'
+import { compare } from 'bcryptjs'
+import { PrismaService } from 'src/infra/database/prisma/prisma.service'
+import z from 'zod'
+
+const authenticateBodySchema = z.object({
+	email: z.email(),
+	password: z.string(),
+})
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
 
 @Controller('/login')
 export class AuthenticateController {
-	constructor(private jwt: JwtService) {}
+	constructor(
+		private prisma: PrismaService,
+		private jwt: JwtService
+	) {}
 
+	@ApiOperation({
+		description: 'Login de usuário na plataforma',
+	})
+	// @ApiResponse({
+	// 	description: 'OK',
+	// 	type: string,
+	// 	status: HttpStatus.OK,
+	// })
 	@Post('/')
-	// @HttpCode(201)
-	async handle() {
-		const token = this.jwt.sign({ sub: 'user-id' })
+	@HttpCode(201)
+	// @UsePipes(new )
+	async handle(@Body() body: AuthenticateBodySchema) {
+		const { email, password } = body
 
-		return token
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email,
+			},
+		})
+
+		if (!user) {
+			throw new UnauthorizedException('Crendenciais invállidas.')
+		}
+
+		const isPasswordValid = await compare(password, user.password)
+
+		if (!isPasswordValid) {
+			throw new UnauthorizedException('Crendenciais invállidas.')
+		}
+
+		const accessToken = this.jwt.sign({ sub: user.id })
+
+		return {
+			access_token: accessToken,
+		}
 	}
 }
