4bc7ac659e4c70cad52e8d8dc2a7554f1496fcb1
Nathan Baldez <nathanbsb2@hotmail.com>
2025-07-25 09:27:30 -0300

    fix: git ignore, problemas com o vitest

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from 'generated/prisma'
+import { PrismaClient } from 'generated/prisma/client'
 import { logsAdapter } from '@/common/utils/logs.adapter'
 
 @Injectable()
