1a70b4141675205d7c04ac631b02629eecf20fe7
Nathan Baldez <nathanbsb2@hotmail.com>
2025-07-30 18:36:19 -0300

    config: setup testes e2e

---
+++ test/setup-e2e.ts
@@ -0,0 +1,45 @@
+import { randomUUID } from 'node:crypto'
+import { envSchema } from '@/infra/env'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from 'generated/prisma/client'
+
+// config({ path: '.env', override: true })
+// config({ path: '.env.test', override: true })
+
+const env = envSchema.parse(process.env)
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+	if (!env.DATABASE_URL) {
+		throw new Error('Please provider a DATABASE_URL environment variable')
+	}
+
+	const url = new URL(env.DATABASE_URL)
+
+	url.searchParams.set('schema', schemaId)
+
+	return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+	const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+	process.env.DATABASE_URL = databaseURL
+
+	// Forma de setar a variável ambiente DATABASE_URL especificamente para o comando de migrations do Prisma
+	execSync(`npx prisma migrate deploy`, {
+		env: { ...process.env, DATABASE_URL: databaseURL },
+		stdio: 'inherit',
+	})
+
+	console.log('Migrações aplicadas para:', process.env.DATABASE_URL)
+})
+
+afterAll(async () => {
+	await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+	await prisma.$disconnect()
+})
