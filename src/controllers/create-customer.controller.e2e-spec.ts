2f13166c2e9d47e0a412e3013f909114a0078764
Nathan Baldez <nathanbsb2@hotmail.com>
2025-07-31 01:22:25 -0300

    feat: testes e2e do create customer controller

--- src/controllers/create-customer.controller.e2e-spec.ts
+++ src/controllers/create-customer.controller.e2e-spec.ts
@@ -1,6 +1,8 @@
 import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { Test, TestingModule } from '@nestjs/testing'
 import { hash } from 'bcryptjs'
+import { Prisma } from 'generated/prisma'
 import * as request from 'supertest'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { PrismaService } from '@/infra/database/prisma/prisma.service'
@@ -10,6 +12,7 @@ import { AppModule } from '../app.module'
 describe('Create Customer Controller (E2E)', () => {
 	let app: INestApplication
 	let prisma: PrismaService
+	let _jwt: JwtService
 
 	beforeAll(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -20,7 +23,9 @@ describe('Create Customer Controller (E2E)', () => {
 			.compile()
 
 		app = module.createNestApplication()
+
 		prisma = module.get(PrismaService)
+		_jwt = module.get(JwtService)
 
 		await app.init()
 	})
@@ -28,7 +33,7 @@ describe('Create Customer Controller (E2E)', () => {
 	test('[POST /customers] Criação de cliente', async () => {
 		const hashedPassword = await hash('123456', 8)
 
-		await prisma.user.create({
+		const userCreated: Prisma.UserCreateInput = await prisma.user.create({
 			data: {
 				name: 'John Doe',
 				email: 'johndoe@email.com',
@@ -36,51 +41,44 @@ describe('Create Customer Controller (E2E)', () => {
 			},
 		})
 
+		const { id } = userCreated
+
 		await request(app.getHttpServer()).post('/login').send({
 			email: 'johndoe@email.com',
 			password: '123456',
 		})
 
+		// const token = jwt.sign({ sub: id })
+
+		MockAuthGuard.userIdToInject = id || ''
+
 		const customerPayload = {
 			name: 'Ana Carolina Silva',
 			email: 'ana.carolina@example.com',
-			phone: '5511987654321', // Exemplo de telefone com DDI e DDD
-			birthDate: '1990-05-15', // Formato YYYY-MM-DD
+			phone: '5511987654321',
+			birthDate: '1990-05-15',
 			street: 'Rua das Flores',
 			number: '123',
 			complement: 'Apto 45',
 			city: 'São Paulo',
 			state: 'SP',
 			country: 'Brasil',
-			zipCode: '01001-000', // Exemplo de CEP
+			zipCode: '01001-000',
 		}
 
-		const response = await request(app.getHttpServer())
+		await request(app.getHttpServer())
 			.post('/customers')
+			// .set('Authorization', `Bearer ${token}`)
 			.send(customerPayload)
 
-		console.log(response.statusCode)
+		const verifyCustomerOnDatabase = await prisma.customer.findUnique({
+			where: {
+				email: customerPayload.email,
+			},
+		})
 
-		// const verifyCustomerOnDatabase = await prisma.customer.findUnique({
-		// 	where: {
-		// 		email: customerPayload.email,
-		// 	},
-		// })
-
-		// console.log(verifyCustomerOnDatabase)
-		// expect(verifyCustomerOnDatabase).toBeTruthy()
+		expect(verifyCustomerOnDatabase).toBeTruthy()
 	})
-
-	// test('[POST /accounts] Deve retornar status 409', () => {
-	// 	return request(app.getHttpServer())
-	// 		.post('/accounts')
-	// 		.send({
-	// 			name: 'John Doe',
-	// 			email: 'johndoe@email.com',
-	// 			password: '123456',
-	// 		})
-	// 		.expect(409)
-	// })
 
 	afterAll(async () => {
 		await app.close()
--- test/utils/mock-auth.guard.ts
+++ test/utils/mock-auth.guard.ts
@@ -3,10 +3,19 @@ import { Observable } from 'rxjs'
 
 @Injectable()
 export class MockAuthGuard implements CanActivate {
+	static userIdToInject: string | null = null
+
 	canActivate(
-		_context: ExecutionContext
+		context: ExecutionContext
 	): boolean | Promise<boolean> | Observable<boolean> {
 		// Sempre retorna true para permitir o acesso
+
+		const request = context.switchToHttp().getRequest()
+
+		request.user = {
+			sub: MockAuthGuard.userIdToInject,
+		}
+
 		return true
 	}
 }
